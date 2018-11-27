d6e4e313b912675d634b0ff09178ec462336adca
Francisca Vasconcelos <francisc@mit.edu>
2018-11-27 00:59:53 -0500

    Fran first commit

--- fran_modules.v
+++ fran_modules.v
@@ -9,7 +9,7 @@ module data_dump(
 
 endmodule // data_dump
 
-module binner(
+module hist2d(
     input clk100,
     input [31:0] i_val, q_val,
     input [3:0] x_bin, y_bin  
@@ -19,11 +19,35 @@ module binner(
 endmodule // data_dump
 
 module classify(
-    input [31:0] i_val, q_val
-    input [31:0] i_pt_1, q_pt_1
-    input [31:0] i_pt_2, q_pt_2
+    input clk100,
+    input signed [31:0] i_val, q_val, // pt to be classified
+    input signed [31:0] i_pt_line, q_pt_line, // pt #1 on classification line
+    // vector from origin with slope perpendicular to line, pts in direction of excited state
+    input signed [31:0] i_vec_perp, q_vec_perp 
+    output 
     );
     
+    reg signed [31:0] i_vec_pt, q_vec_pt; // vector from origin to pt to classify
+
+    reg signed [63:0] dot_product;
+
+    always @(posedge clk100) begin
+        i_vec_pt <= i_val - i_pt_line;
+        q_vec_pt <= q_val - q_pt_line;
+
+        // basic implementation, can be improved by looking just at signs/with tricks
+        dot_product = i_vec_pt*i_vec_perp + q_vec_pt*q_vec_perp;
+
+        // EXCITED STATE CLASSIFICATION
+        if(dot_product>1'b0) begin
+        end 
+        // GROUND STATE CLASSIFICATION
+        else if (dot_product<1'b0) begin
+        end
+        // PT ON CLASSIFIER
+        else begin 
+        end
+    end
 
 endmodule // data_dump
 
@@ -41,11 +65,11 @@ module analyze_fsm(
     // define states
     parameter DATA_DUMP_MODE = 2'b00;
     parameter CLASSIFY_MODE = 2'b01
-    parameter BIN_MODE = 2'b11;
+    parameter HIST2D_MODE = 2'b11;
     
 
     // analysis FSM
-    always @(posedge clk100)
+    always @(posedge clk100) begin
         case(state)
             DATA_DUMP_MODE: begin
 
@@ -55,7 +79,7 @@ module analyze_fsm(
 
             end // DATA_DUMP_MODE
 
-            BIN_MODE: begin
+            HIST2D_MODE: begin
 
             end // DATA_DUMP_MODE
 
@@ -65,10 +89,10 @@ module analyze_fsm(
     end
     
     // instantiate analysis modules
-    data_dump dump();
+    data_dump dump(.clk100(clk100),);
 
-    binner bin();
+    hist2d hist(.clk100(clk100),);
 
-    classify class();
+    classify class(.clk100(clk100),);
 
 endmodule // data_dump
