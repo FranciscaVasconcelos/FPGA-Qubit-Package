f9d062e00e32c4171d12a16fec511a5871c304dd
Megan Yamoah <myamoah@mit.edu>
2018-11-27 23:26:17 -0500

    DDS and default bin parameters

--- top_modules.v
+++ top_modules.v
@@ -5,6 +5,7 @@
 ////////////////////////////////////////////////////////////////////////////////
 
 module config_params(
+    // inputs
     input clk100,
     input reset,
     input config_reset,
@@ -13,8 +14,19 @@ module config_params(
     input [5:0] sample_freq_new, // min 8 MHz
     input [9:0] delay_time_new, // max 10 us
     input [1:0] analyze_mode_new, // data dump, binning, classifier
-    input [3:0] x_bin_new,
-    input [3:0] y_bin_new,
+    input [15:0] x_bin_width_new, // width of bin in x_direction 
+    input [15:0] y_bin_width_new, // width of bin in y_direction 
+    input [4:0] x_bin_num_new, // number of bins along x_direction 
+    input [4:0] y_bin_num_new, // number of bins along y_direction 
+    input signed [15:0] x_bin_min_new, // start of bins x_direction 
+    input signed [15:0] y_bin_min_new, // start of bins y_direction
+    // outputs
+    output reg [15:0] x_bin_width, // width of bin in x_direction 
+    output reg [15:0] y_bin_width, // width of bin in y_direction 
+    output reg [4:0] x_bin_num, // number of bins along x_direction 
+    output reg [4:0] y_bin_num, // number of bins along y_direction 
+    output reg signed [15:0] x_bin_min, // start of bins x_direction 
+    output reg signed [15:0] y_bin_min, // start of bins y_direction 
     output reg [3:0] demod_freq, // freq divided by 10 MHz
     output reg [10:0] sample_length, // max 20 us
     output reg [5:0] sample_freq, // min 8 MHz
@@ -30,18 +42,26 @@ module config_params(
             sample_freq <= 6'd5; // 100 MHz
             delay_time <= 14'd5000;
             analyze_mode <= 2'd0;
-            x_bin <= 3'd10;
-            y_bin <= 3'd10;
-        end // if (reset)
+            x_bin_width <= 16'd100;
+            y_bin_width <= 16'd100;
+            x_bin_num <= 5'd10;
+            y_bin_num <= 5'd10;
+            x_bin_min <= 16'd0;
+            y_bin_min <= 16'd0; 
+        end
         else if (config_reset) begin
             demod_freq <= demod_freq_new;
             sample_length <= sample_length_new;
             sample_freq <= sample_freq_new;
             delay_time <= delay_time_new;
             analyze_mode <= analyze_mode_new;
-            x_bin <= x_bin_new;
-            y_bin <= y_bin_new;
-        end // if (config_reset_new)
+            x_bin_width <= x_bin_width_new;
+            y_bin_width <= y_bin_width_new;
+            x_bin_num <= x_bin_num_new;
+            y_bin_num <= y_bin_num_new;
+            x_bin_min <= x_bin_min_new;
+            y_bin_min <= y_bin_min_new; 
+        end
     end
 
 endmodule // config
@@ -83,7 +103,7 @@ module timing(
                     counter <= counter + 1;
                 else if (~reset && counter == delay) begin
                     counter <= 0;
-                    start_collect <= 1;
+                    start_collect <= 1; // asserted on same clock cycle of first value to sample
                     state <= IDLE;
                 end
             end
@@ -130,15 +150,16 @@ module sampler(
             phase_vals <= {5{14'b0}};
         end
 
+        // make sure data values match phase_vals
         data_i_out <= data_i_in;
         data_q_out <= data_q_in;
 
         case (state) begin
             IDLE: begin
-                if (start) begin
+                if (start) begin // from timing, assert on same clock sample as value to sample
                     state <= SAMPLE;
                     counter <= counter + 1;
-                    phase_vals[1] <= (1 % sample_skip == 0) ? demod_freq : 0;
+                    phase_vals[1] <= (1 % sample_skip == 0) ? demod_freq : 0; // phase_vals set at next clock cycle
                     phase_vals[2] <= (2 % sample_skip == 0) ? 2*demod_freq : 0;
                     phase_vals[3] <= (3 % sample_skip == 0) ? 3*demod_freq : 0;
                     phase_vals[4] <= (4 % sample_skip == 0) ? 4*demod_freq : 0;
@@ -172,14 +193,6 @@ module sampler(
 
 endmodule // sampler
 
-
-dds_compiler_0 sin_cos(.aclk(clk100), .s_axis_phase_tvalid(phase_valid),
-    .s_axis_phase_tdata : IN STD_LOGIC_VECTOR(7 DOWNTO 0);
-    m_axis_data_tvalid : OUT STD_LOGIC;
-    m_axis_data_tdata : OUT STD_LOGIC_VECTOR(31 DOWNTO 0);
-    event_phase_in_invalid : OUT STD_LOGIC
-  );
-
 module multiplier(
     input clk100,
     input reset,
@@ -191,11 +204,49 @@ module multiplier(
 
     wire signed [15:0] sin_theta [4:0];
     wire signed [15:0] cos_theta [4:0];
+    wire [31:0] sin_cos [4:0];
 
-    assign sin_theta = //dds using phase_vals;
-    assign cos_theta = //dds using phase_vals;
+    wire [7:0] phase_vals_mod [4:0];
+
+    integer j;
+
+    generate
+        for (j = 0; j < 5; j = j + 1) begin
+            assign sin_cos[i] = {sin_theta[i], cos_theta[i]};
+        end
+    endgenerate
+
+    wire phase_valid = 1;
+
+    // DDS COMPILERS
+    // mode: sine/cosine LUT
+    // input phase: 8 bit
+    // output {sine, cosine}: 32 bit
+    // latency: 1
+    dds_compiler_0 sin_cos0(.aclk(clk100), .s_axis_phase_tvalid(phase_valid),
+        .s_axis_phase_tdata(phase_vals_mod[0]),
+        .m_axis_data_tvalid(data_valid), .m_axis_data_tdata(sin_cos[0]),
+        .event_phase_in_invalid(error));
+    dds_compiler_0 sin_cos1(.aclk(clk100), .s_axis_phase_tvalid(phase_valid),
+        .s_axis_phase_tdata(phase_vals_mod[1]),
+        .m_axis_data_tvalid(data_valid), .m_axis_data_tdata(sin_cos[1]),
+        .event_phase_in_invalid(error));
+    dds_compiler_0 sin_cos2(.aclk(clk100), .s_axis_phase_tvalid(phase_valid),
+        .s_axis_phase_tdata(phase_vals_mod[2]),
+        .m_axis_data_tvalid(data_valid), .m_axis_data_tdata(sin_cos[2]),
+        .event_phase_in_invalid(error));
+    dds_compiler_0 sin_cos3(.aclk(clk100), .s_axis_phase_tvalid(phase_valid),
+        .s_axis_phase_tdata(phase_vals_mod[3]),
+        .m_axis_data_tvalid(data_valid), .m_axis_data_tdata(sin_cos[3]),
+        .event_phase_in_invalid(error));
+    dds_compiler_0 sin_cos4(.aclk(clk100), .s_axis_phase_tvalid(phase_valid),
+        .s_axis_phase_tdata(phase_vals_mod[4]),
+        .m_axis_data_tvalid(data_valid), .m_axis_data_tdata(sin_cos[4]),
+        .event_phase_in_invalid(error));
 
     integer i;
+    integer k;
+
     always @(posedge clk100) begin
         if (reset) begin
             data_i_rot <= {5{16'b0}};
@@ -208,6 +259,10 @@ module multiplier(
                     data_q_rot[i] <= data_q_in[i]*cos_theta[i] - data_i_in[i]*sin_theta[i];
                 end
             end
+        end
+
+        for (k = 0; k < 5; k = k+1) begin
+            phase_vals_mod[i] <= phase_vals[i] % 50;
         end
     end
 
