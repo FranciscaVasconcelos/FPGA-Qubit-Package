3e72273c09b265660ac5859466720ba19899b64f
Megan Yamoah <myamoah@mit.edu>
2018-12-06 18:40:54 -0500

    cleaning demod_main

--- demod_main.v
+++ demod_main.v
@@ -39,46 +39,6 @@ module demod_main(
     input [15:0] data1_in_sdi_dataStreamFCx5_S_data_4,
     input data1_in_sdi_dataStreamFCx5_S_valid,
 
-    // Input 2
-    /*input [15:0] data2_in_sdi_dataStreamFCx5_S_data_0,
-    input [15:0] data2_in_sdi_dataStreamFCx5_S_data_1,
-    input [15:0] data2_in_sdi_dataStreamFCx5_S_data_2,
-    input [15:0] data2_in_sdi_dataStreamFCx5_S_data_3,
-    input [15:0] data2_in_sdi_dataStreamFCx5_S_data_4,
-    input data2_in_sdi_dataStreamFCx5_S_valid,
-
-    // Input 3
-    input [15:0] data3_in_sdi_dataStreamFCx5_S_data_0,
-    input [15:0] data3_in_sdi_dataStreamFCx5_S_data_1,
-    input [15:0] data3_in_sdi_dataStreamFCx5_S_data_2,
-    input [15:0] data3_in_sdi_dataStreamFCx5_S_data_3,
-    input [15:0] data3_in_sdi_dataStreamFCx5_S_data_4,
-    input data3_in_sdi_dataStreamFCx5_S_valid,*/
-
-    // Output 0
-    /*output [15:0] data0_out_sdi_dataStreamFCx5_M_data_0,
-    output [15:0] data0_out_sdi_dataStreamFCx5_M_data_1,
-    output [15:0] data0_out_sdi_dataStreamFCx5_M_data_2,
-    output [15:0] data0_out_sdi_dataStreamFCx5_M_data_3,
-    output [15:0] data0_out_sdi_dataStreamFCx5_M_data_4,
-    output data0_out_sdi_dataStreamFCx5_M_valid,
-
-    // Output 1
-    output [15:0] data1_out_sdi_dataStreamFCx5_M_data_0,
-    output [15:0] data1_out_sdi_dataStreamFCx5_M_data_1,
-    output [15:0] data1_out_sdi_dataStreamFCx5_M_data_2,
-    output [15:0] data1_out_sdi_dataStreamFCx5_M_data_3,
-    output [15:0] data1_out_sdi_dataStreamFCx5_M_data_4,
-    output data1_out_sdi_dataStreamFCx5_M_valid,
-
-    // Output 2
-    output [15:0] data2_out_sdi_dataStreamFCx5_M_data_0,
-    output [15:0] data2_out_sdi_dataStreamFCx5_M_data_1,
-    output [15:0] data2_out_sdi_dataStreamFCx5_M_data_2,
-    output [15:0] data2_out_sdi_dataStreamFCx5_M_data_3,
-    output [15:0] data2_out_sdi_dataStreamFCx5_M_data_4,
-    output data2_out_sdi_dataStreamFCx5_M_valid,*/
-
     // Output 3
     output [15:0] data3_out_sdi_dataStreamFCx5_M_data_0,
     output [15:0] data3_out_sdi_dataStreamFCx5_M_data_1,
@@ -87,8 +47,7 @@ module demod_main(
     output [15:0] data3_out_sdi_dataStreamFCx5_M_data_4,
     output data3_out_sdi_dataStreamFCx5_M_valid,
 
-    // Triggers out
-    //output [4:0] trigger0_out, trigger1_out, trigger2_out, trigger3_out
+    // Trigger out
     output [4:0] trigger3_out
 
     );
